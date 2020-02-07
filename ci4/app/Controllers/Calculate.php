1031e8adb4874ededf90cda893655a684c0fb28a
Griffin Rades <ggrade16@smumn.edu>
2020-02-07 16:28:42 -0600

    small changes

--- ci4/app/Controllers/Calculate.php
+++ ci4/app/Controllers/Calculate.php
@@ -2,27 +2,24 @@
 
 class Calculate extends BaseController
 {
-	// public function index()
-	// {
-	// 	return view('calculator');
-	// }
+	//get session data for the user when loaded/reloaded.
 	public function getSession(){
 		$currentTotal = $this->session->get("currentScreen");
 
 		return $this->response->setJSON(array("answer"=>$currentTotal));
 	}
-
+	// handle every number clicked, add info to user session.
 	public function number($number){
 		$currentScreen += $number;
 		$this->session->set("currentScreen", $currentScreen);
 	}
-
+	//hande when the add button is clicked
 	public function add($numToAdd, $currentTotal){
 		$currentTotal += $numToAdd;
 
 		return $this->response->setJSON(array("answer"=>$currentTotal));
 	}
-
+	//hanle when the subtraction button is clicked
 	public function subtract($numToSubtract, $currentTotal, $first = false){
 		if($first){
 			$currentTotal = $numToSubtract;
@@ -31,13 +28,13 @@ class Calculate extends BaseController
 		}
 		return $this->response->setJSON(array("answer"=>$currentTotal));
 	}
-
+	//handle when the mutliply button is clicked
 	public function multiply($number, $currentTotal){
 		$currentTotal *= $number;
 
 		return $this->response->setJSON(array("answer"=>$currentTotal));
 	}
-
+	//hadle when the divide button is clicked
 	public function divide($number, $currentTotal){
 		if($currentTotal != "0"){
 			$currentTotal = $currentTotal / $number;
@@ -47,7 +44,7 @@ class Calculate extends BaseController
 
 		return $this->response->setJSON(array("answer"=>$currentTotal));
 	}
-
+	//handle when the equal button is clicked
 	public function equal($total, $prevOp, $number){
 		if($prevOp == "add"){
 			$total += $number;
@@ -57,18 +54,21 @@ class Calculate extends BaseController
 			$total *= $number;
 		}elseif($prevOp == "divide"){
 			if($total != 0){
-					$total = $total / $number;
+				$total = $total / $number;
 			}
+		}elseif($prevOp == "none"){
+			$total = $total;
 		}
 
 		return $this->response->setJSON(array("answer"=>$total));
 	}
-
+	//handle when the precent button is clicked
 	public function percent($number){
 		$number = $number * .01;
 
 		return $this->response->setJSON(array("answer"=>$number));
 	}
+	//handle when the clear button is clicked
 	public function clear(){
 		$this->session->set('currentScreen', "");
 	}
--- ci4/public/script.js
+++ ci4/public/script.js
@@ -1,11 +1,11 @@
 window.addEventListener("DOMContentLoaded", loaded);
-var currentTotal = 0;
-var first = true;
-var previousOp = "";
-var numNext = true;
+var currentTotal = 0;  //keep track of calculation total
+var first = true; //keep track to if it is the first time subtract button is clicked
+var previousOp = "none"; //keep track of the previous operation for when the equal button is clicked
+var numNext = true; //keep track of when to append or start a new number input
 
 function loaded(){
-
+  //retrievs the current users data on load/reload.
   $.ajax({
     type: "POST",
     url: "calculate/getSession",
@@ -20,6 +20,7 @@ function loaded(){
     }
   });
 
+  //connect the variables and constants to their calculator button or the display
   var display = document.getElementById("calcDisplay");
   var numberButtons = document.getElementsByClassName("number");
   const addButton = document.getElementById("addition");
@@ -32,6 +33,7 @@ function loaded(){
   const clearButton = document.getElementById("clear");
   const negateButton = document.getElementById("negate");
 
+  //loop through the numberButton and give them and event listener
   for(var i = 0; i < numberButtons.length; i++){
     numberButtons[i].addEventListener("click", function(e){
       if(numNext){
@@ -44,7 +46,7 @@ function loaded(){
         display.innerHTML = e.target.value;
         numNext = true;
       }
-
+      //call server to update the value of the current display. Used for session data
       $.ajax({
         type: "POST",
         url: "calculate/number/" + display.innerHTML,
@@ -56,7 +58,8 @@ function loaded(){
       });
     });
   }
-
+  //add operation button clicked. Call to the server calculate/add/'numberOnScreen'/'currentTotal'
+  //response: set currentTotal to new total, change screen to operation, set previousOp, and set numNext to false.
   addButton.addEventListener("click", function(){
     $.ajax({
       type: "POST",
@@ -70,8 +73,10 @@ function loaded(){
       }
     });
   });
-
+  //subtract operation button clicked. Call to the server calculate/subtract/'numberOnScreen'/'currentTotal'/'ifFirstTimeSubtractClicked'
+  //response: set currentTotal to new total, change screen to operation, set previousOp, and set numNext to false.
   subtractButton.addEventListener("click", function(){
+    //if it is the first time that subtract was clicked send that information to the server.
     if(first){
       $.ajax({
         type: "POST",
@@ -84,7 +89,7 @@ function loaded(){
           numNext = false;
         }
       });
-      first = false;
+      first = false; //subtract has been clicked
     }else{
       $.ajax({
         type: "POST",
@@ -99,36 +104,37 @@ function loaded(){
       });
     }
   });
-
+  //multiply operation button clicked. Call to the server calculate/multiply/'numberOnScreen'/'currentTotal'
+  //response: set currentTotal to new total, change screen to operation, set previousOp, and set numNext to false.
   mutlButton.addEventListener("click", function(){
-      if(currentTotal == 0){
-          $.ajax({
-            type: "POST",
-            url: "calculate/multiply/" + display.innerHTML + "/" + 1,
-            dataType:'JSON',
-            success: function(response){
-              currentTotal = response.answer;
-              display.innerHTML = "*";
-              previousOp = "multiply";
-              numNext = false;
-            }
-          });
-      }else{
-          $.ajax({
-            type: "POST",
-            url: "calculate/multiply/" + display.innerHTML + "/" + currentTotal,
-            dataType:'JSON',
-            success: function(response){
-              currentTotal = response.answer;
-              display.innerHTML = "*";
-              previousOp = "multiply";
-              numNext = false;
-            }
-          });
-      }
-
+    if(currentTotal == 0){
+      $.ajax({
+        type: "POST",
+        url: "calculate/multiply/" + display.innerHTML + "/" + 1,
+        dataType:'JSON',
+        success: function(response){
+          currentTotal = response.answer;
+          display.innerHTML = "*";
+          previousOp = "multiply";
+          numNext = false;
+        }
+      });
+    }else{
+      $.ajax({
+        type: "POST",
+        url: "calculate/multiply/" + display.innerHTML + "/" + currentTotal,
+        dataType:'JSON',
+        success: function(response){
+          currentTotal = response.answer;
+          display.innerHTML = "*";
+          previousOp = "multiply";
+          numNext = false;
+        }
+      });
+    }
   });
-
+  //divide operation button clicked. Call to the server calculate/divide/'numberOnScreen'/'currentTotal'
+  //response: set currentTotal to new total, change screen to operation, set previousOp, and set numNext to false.
   divideButton.addEventListener("click", function(){
     $.ajax({
       type: "POST",
@@ -142,7 +148,8 @@ function loaded(){
       }
     });
   });
-
+  //percent operation button clicked. Call to the server calculate/percent/'numberOnScreen'
+  //response: set currentTotal to 0, set display to response, and set numNext to false.
   percentButton.addEventListener("click", function(){
     $.ajax({
       type: "POST",
@@ -155,44 +162,42 @@ function loaded(){
       }
     });
   });
-
+  //decimal operation button clicked.
   decimalButton.addEventListener("click", function(){
-    if(!display.innerHTML.includes(".")){
+    if(!display.innerHTML.includes(".")){ //if display does not have a decimal add one to the end of the display
       display.innerHTML += ".";
     }
   });
-
+  //negate operation button clicked.
   negateButton.addEventListener("click", function(){
-    if(!display.innerHTML.includes("-") && display.innerHTML != 0){
+    if(!display.innerHTML.includes("-") && display.innerHTML != 0){ //if display is not 0 and it is not alread negative add negative sign to front
       display.innerHTML = "-" + display.innerHTML;
-    }else if(display.innerHTML.includes("-")){
+    }else if(display.innerHTML.includes("-")){ //if already is negative remove the negative
       display.innerHTML = display.innerHTML.substring(1);
     }
   });
-
+  //equal operation button clicked. Call to the server calculate/equal/'numberOnScreen'/previousOperation'/'currentTotal'
+  //response: set currentTotal to 0, change screen to response, set previousOp to none, and set numNext to false.
   equalButton.addEventListener("click", function(){
     $.ajax({
       type: "POST",
       url: "calculate/equal/" + currentTotal + "/" + previousOp + "/" + display.innerHTML,
       dataType:'JSON',
       success: function(response){
-        currentTotal = response.answer;
-        display.innerHTML = currentTotal;
+        display.innerHTML = response.answer;
         currentTotal = 0;
         previousOp = "";
         numNext = false;
       }
     });
   });
-
+  //clear operation button clicked. Call to the server calculate/clear  used to remove session data.
+  //set currentTotal to 0, change screen to 0, and set numNext to true.
   clearButton.addEventListener("click", function(){
     $.ajax({
       type: "POST",
       url: "calculate/clear",
       dataType:'JSON',
-      success: function(response){
-        console.log("Cleared");
-      }
     });
     numNext = true;
     currentTotal = 0;
