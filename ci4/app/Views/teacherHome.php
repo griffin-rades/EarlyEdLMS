c4f3d4ff20166564d0becf1a29888f65f52b9946
Griffin Rades <ggrade16@smumn.edu>
2020-02-23 21:53:33 -0600

    small changes

--- ci4/app/Views/teacherHome.php
+++ ci4/app/Views/teacherHome.php
@@ -16,7 +16,7 @@
 								</div>
 								<div class="card-body">
 									<p>Hello: <?php echo $this->data['teacherName'];?></p>
-									<p>Edit Account</p>
+									<a href="#">Edit Account</a>
 								</div>
 							</div>
 						</div>
@@ -25,10 +25,18 @@
 						<div class="card studentDetailsHome">
 							<div class="card-body">
 								<div class="card-header">
-									<h3>All <?php echo $this->data['teacherName'];?> Student Grades</h3>
+									<h3>All Student Grades</h3>
 								</div>
 								<ol>
 									<div class="container-fluid studentList">
+										<div class="row">
+											<div class="col-6">
+												<h5 style="text-decoration: underline">Name</h5>
+											</div>
+											<div class="col-6">
+												<h5 style="text-decoration: underline">Grade/100</h5>
+											</div>
+										</div>
 									<?php foreach($this->data['studentGradeList'] as $row){
 										echo "<div class='row'>";
 										echo "<div class='col-6'> " . $row->studentFirstName . " " . $row->studentLastName . "</div><div class='col-6'>" . $row->grade . "</div>";
